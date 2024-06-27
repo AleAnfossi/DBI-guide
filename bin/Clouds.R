c771f72ee1a5461c88bc0206333d85c708d0e70c
AleAnfossi <a.anfossi@campus.unimib.it>
2024-06-27 18:58:17 +0200

    Ended clouds of data tests
    I saved the results and the generated data after having checked them twice

--- bin/Clouds.R
+++ bin/Clouds.R
@@ -6,12 +6,12 @@
 
 
 #Cardinality of each initial cluster 
-number<-100   #<------------
+number<-10000   #<------------
 
 #Dataset creation single cloud of 2*number
-mydata<-SingCloud(number)[, 1:2]
+#mydata<-SingCloud(number)[, 1:2]
 #Dataset creation seperate clouds of number
-#mydata<-SepClouds(number)[, 1:2]
+mydata<-SepClouds(number)[, 1:2]
 
 #Executing kmeans clustering
 kmeans_result <- kmeans(mydata, centers = 2, iter.max=20, nstart = 25)
@@ -23,10 +23,10 @@ mydata$kmeans_cluster <- factor(kmeans_result$cluster)
 indexes<-DBI(mydata[,1:2],mydata[,3])
 
 #Add name for saving!! This is the data csv print
-write.csv(mydata, file="10sing_data.csv")  #<------------
+write.csv(mydata, file="10000sep_data.csv")  #<------------
 
 #Add name for saving!! This is the DBI evaluation csv print
-write.csv(indexes, file="10sing_DBI.csv") #<-----------
+write.csv(indexes, file="10000sep_DBI.csv") #<-----------
 
 # Plot the kmeans clusters
 ggplot(mydata, aes(x = x, y = y, color = kmeans_cluster)) +
